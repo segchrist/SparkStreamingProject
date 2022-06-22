85c64d8bff9a4db6404a51ddbd3cf1c3b8fce026
Gloriane Christelle SEGOUN <glorianesegoun@MacBook-Pro-de-Gloriane.local>
2022-06-22 23:11:01 +0200

    First commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "SparkStreamingProject"
+
+version := "0.1"
+
+scalaVersion := "2.13.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.2.1"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.2.1"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "3.2.1"
+
---
+++ src/main/scala/main.scala
@@ -0,0 +1,43 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object main extends App {
+  val sparkSession = SparkSession
+    .builder()
+    .master("local[*]")
+    .appName("streaming")
+    .getOrCreate()
+
+  sparkSession.sparkContext.setLogLevel("ERROR")
+  sparkSession.conf.set("spark.sql.shuffle.partitions","5")
+
+  val vlibdf = sparkSession
+    .read
+    .format("csv")
+    .option("inferSchema","true")
+    .option("header","true")
+    .option("delimiter", ";")
+    .load("src/files/vlille-realtime.csv")
+
+  // definir le schema pour le streaming
+  val vlibSchema = vlibdf.schema
+
+  val vlibStream = sparkSession.readStream
+    .schema(vlibSchema)
+    .option("maxFilesPerTrigger",1)
+    .format("csv")
+    .option("header","true")
+    .option("delimiter", ";")
+    .load("src/files/vlille-realtime.csv")
+
+  println("V Lille is streaming: " + vlibStream.isStreaming)
+  vlibdf.show()
+  // Aggregation
+  /*
+  vlibdf.select(count("StockCode")).show(false)
+  vlibdf.select(countDistinct("StockCode")).show(false)
+  vlibdf.select(approx_count_distinct("StockCode", 0.02)).show(false)
+  vlibdf.select(first("StockCode"), last("StockCode")).show(false)
+  */
+
+}
