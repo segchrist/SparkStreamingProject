6cf6925049420e3ca2b70c5fa50fc562c9799194
Gloriane Christelle SEGOUN <glorianesegoun@MacBook-Pro-de-Gloriane.local>
2022-07-05 22:16:28 +0200

    commit du 5 juillet2022

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -17,21 +17,99 @@ object main extends App {
     .option("inferSchema","true")
     .option("header","true")
     .option("delimiter", ";")
-    .load("src/files/vlille-realtime.csv")
+    .load("src/files/velib_renamed.csv")
 
   // definir le schema pour le streaming
   val vlibSchema = vlibdf.schema
 
+  vlibdf.show(10)
+
+  // definir Stream
   val vlibStream = sparkSession.readStream
     .schema(vlibSchema)
     .option("maxFilesPerTrigger",1)
     .format("csv")
     .option("header","true")
     .option("delimiter", ";")
-    .load("src/files/vlille-realtime.csv")
+    .load("src/files/velib_renamed*.csv")
 
-  println("V Lille is streaming: " + vlibStream.isStreaming)
-  vlibdf.show()
+  println("VLib is streaming: " + vlibStream.isStreaming)
+
+
+
+  // Moyenne velos disponible, velos mecaniques, velos electriques par commune
+  println("Moyenne velos disponible, velos mecaniques, velos electriques par commune")
+
+  val NbAvailableBikesByMunicipality = vlibStream
+    .selectExpr("Municipality",
+      "TotalNumberOfBike",
+      "AvailableMechanicBikes",
+      "AvailableElectricBikes",
+      "FreeParking")
+    .groupBy(
+      col("Municipality")
+    )
+    .sum()
+  NbAvailableBikesByMunicipality.writeStream
+    .format("console") // memory = sotre in-memory table
+    .queryName("nbAvailableBikesByMunicipality_stream") // the name of the in-memory table
+    .outputMode("complete") // complete = all the counts should be in the table
+    .start()
+
+
+
+//Top 5
+  println("Top 5 velos disponible, velos mecaniques, velos electriques par commune")
+  val Top5BikesByMunicipality = vlibStream
+    .selectExpr("Municipality",
+      "LastUpdate" ,"TotalNumberOfBike"
+      )
+    .groupBy(
+      col("Municipality"), window(col("LastUpdate"), "1 hour")
+    )
+    .sum()
+    .orderBy()
+
+  Top5BikesByMunicipality.writeStream
+    .format("console") // memory = sotre in-memory table
+    .queryName("Top5BikesByMunicipality_stream") // the name of the in-memory table
+    .outputMode("complete") // complete = all the counts should be in the table
+    .start()
+
+  /*for (i <- 1 to 5) {
+    sparkSession.sql(
+      """
+        |select * from nbAvailableBikesByMunicipality_stream
+        |""".stripMargin)
+      .show(false)
+    Thread.sleep(1000)
+  }*/
+
+
+  //Nb station par commune
+  println("Nb stations par commune")
+  val NbStationByMuni = vlibStream
+    .selectExpr(  "Municipality"  )
+    .groupBy(
+      col("Municipality")
+    )
+    .count()
+
+
+  NbStationByMuni.writeStream
+    .format("memory") // memory = sotre in-memory table
+    .queryName("NbStationByMuni_stream") // the name of the in-memory table
+    .outputMode("complete") // complete = all the counts should be in the table
+    .start()
+  for (i <- 1 to 3) {
+    sparkSession.sql(
+      """
+        |select * from NbStationByMuni_stream
+      """.stripMargin)
+      .show(false)
+    Thread.sleep(1000)
+  }
+
   // Aggregation
   /*
   vlibdf.select(count("StockCode")).show(false)
