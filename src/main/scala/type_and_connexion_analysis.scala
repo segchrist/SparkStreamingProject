5b84fc5f30050349be8de6d24e83324d9e93ed72
badje <badjemm@3il.fr>
2022-07-06 02:07:14 +0200

    all request
    Signed-off-by: badje <badjemm@3il.fr>

--- src/main/scala/type_and_connexion_analysis.scala
+++ src/main/scala/type_and_connexion_analysis.scala
@@ -38,7 +38,107 @@ object type_and_connexion_analysis extends App {
   println("VLib is streaming: " + vlibStream.isStreaming)
   //vlibdf.show()
 
-  val elBikepermunicipality = vlibStream
+  // NOMBRE MOYEN DE VELO DISPO PAR JOUR DANS CHAQUE COMMUNE
+  /*val avgNbBikePerDay = vlibStream
+    .selectExpr("Municipality", "TotalNumberOfBike", "LastUpdate")
+    .groupBy(col("Municipality"), window(col("LastUpdate"), "1 day"))
+    .avg("TotalNumberOfBike")
+    .withColumn("AvgNbBike",round(col("avg(TotalNumberOfBike)"),1))
+    .drop(col("avg(TotalNumberOfBike)"))
+    .orderBy("AvgNbBike")*/
+
+// NOMBRE TOTAL DE VELO DISPO PAR TRANCHE HORAIRE DE DEUX HEURES
+ /* val availableBikes = vlibStream
+    .selectExpr("Municipality", "TotalNumberOfBike", "LastUpdate")
+    .groupBy(col("Municipality"), window(col("LastUpdate"), "2 hour","1 hour"))
+    .sum("TotalNumberOfBike")
+    .withColumnRenamed("Sum(TotalNumberOfBike)","AvailableBikes")
+    .orderBy("AvailableBikes")*/
+
+  // NOMBRE MOYEN DE VELO DISPO PAR JOUR DANS CHAQUE COMMUNE POUR CHAQUE TYPE DE VELO
+/*  val avgNbBikePerTypePerDay = vlibStream
+    .selectExpr("Municipality", "AvailableMechanicBikes", "LastUpdate","AvailableElectricBikes")
+    .groupBy(col("Municipality"), window(col("LastUpdate"), "1 day"))
+    .avg("AvailableMechanicBikes","AvailableElectricBikes")
+    .withColumn("AvgNbMechBike",round(col("avg(AvailableMechanicBikes)"),1))
+    .withColumn("AvgNbElecBike",round(col("avg(AvailableElectricBikes)"),1))
+    .drop(col("avg(AvailableElectricBikes)"))
+    .drop(col("avg(AvailableMechanicBikes)"))
+    .orderBy("AvgNbElecBike","AvgNbMechBike")*/
+
+  // NOMBRE TOTAL DE VELO DISPO PAR TRANCHE HORAIRE DE DEUX HEURES
+  /* val availableBikes = vlibStream
+     .selectExpr("Municipality", "TotalNumberOfBike", "LastUpdate")
+     .groupBy(col("Municipality"), window(col("LastUpdate"), "2 hour","1 hour"))
+     .sum("TotalNumberOfBike")
+     .withColumnRenamed("Sum(TotalNumberOfBike)","AvailableBikes")
+     .orderBy("AvailableBikes")*/
+
+  //POURCENTAGE DE STATION EN FONCTIONNEMENT
+//  val numStationPerCom = vlibStream
+//    .selectExpr("ID","Municipality")
+//    .groupBy("Municipality")
+//    .count()
+//    .withColumnRenamed("count","nbBikePerCom")
+//  val StationPartByMuni = vlibStream
+//    .selectExpr( "Working", "Municipality" )
+//    .groupBy(
+//      col("Municipality"),
+//      col("Working")
+//    )
+//    .count()
+//
+//  val percentStationByMunic = StationPartByMuni
+//    .join(numStationPerCom,"Municipality")
+//    //.withColumn("percentWorkingStationByMunic",round(col("count")/col("nbBikePerCom"),1))
+
+  //POURCENTAGE DE STATION AVEEC RETOUR POSSIBLE
+  val StationPartWithReturnByMuni = vlibStream
+    .selectExpr("Municipality", "VelibPossibleReturn" )
+    .groupBy(
+      col("VelibPossibleReturn"),
+      col("Municipality")
+    )
+    .count()
+
+
+
+
+  /*
+  val ms_velomin = ms_velo
+    .selectExpr("Municipality", "window", "nbbike")
+    .groupBy("Municipality")
+    .agg(min("nbbike"))*/
+
+  StationPartWithReturnByMuni.writeStream
+    .format("console") // memory = sotre in-memory table
+    .trigger(Trigger.ProcessingTime("10 seconds"))
+    .option("truncate", "false")
+    //.queryName("velo_dispo") // the name of the in-memory table
+    .outputMode("complete")
+    .start()
+    .awaitTermination()
+/*
+  val NbAvailableBikesByMunicipality = vlibStream
+    .selectExpr("Municipality",
+      "TotalNumberOfBike",
+      "AvailableMechanicBikes",
+      "AvailableElectricBikes",
+      "FreeParking")
+    .groupBy(
+      col("Municipality")
+    )
+    .sum()
+  NbAvailableBikesByMunicipality.writeStream
+    .format("console")
+    .queryName("nbAvailableBikesByMunicipality_stream")
+    .outputMode("complete")
+    .trigger(Trigger.ProcessingTime("10 seconds"))
+    .start()
+    .awaitTermination()
+*/
+
+/*  val elBikepermunicipality = vlibStream
     .selectExpr(
       "Municipality",
       "AvailableElectricBikes",
@@ -46,7 +146,7 @@ object type_and_connexion_analysis extends App {
       "LastUpdate"
     )
     .groupBy(
-      col("Municipality"), window(col("LastUpdate"),"1 hour"),col("TotalNumberOfBike")
+      col("Municipality"), window(col("LastUpdate"),"2 hours"),col("TotalNumberOfBike")
     )
     .sum("AvailableElectricBikes")
     .withColumnRenamed("sum(AvailableElectricBikes)","nbElectricBike")
@@ -58,9 +158,9 @@ object type_and_connexion_analysis extends App {
     .format("console") // memory = store in-memory table
     //.queryName("availableBikePerMunicipality") // the name of the in-memory table
     //.option("checkpointlocation","src/checkpoint")
-    .outputMode("append") // complete = all the counts should be in the table
+    .outputMode("complete") // complete = all the counts should be in the table
     .start()
-  query.awaitTermination()
+  query.awaitTermination()*/
 
 
 /*  for (i <- 1 to 50 ) {
